deecc941d92aeb0bcacd91fb1d1348aa9692a2c6
priusz <varro.timi@gmail.com>
2024-11-05 14:21:24 +0100

    add logout route, controller + method, confirm option

--- laravel-app/resources/js/app.js
+++ laravel-app/resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+document.addEventListener('DOMContentLoaded', function () {
+
+    //logout confirm
+    const logoutButton = document.getElementById('logout-button');
+    logoutButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        const logoutUrl = this.getAttribute('data-url');
+        if (confirm("Are you sure you want to log out?")) {
+            window.location.href = logoutUrl;
+        }
+    })
+})
+
