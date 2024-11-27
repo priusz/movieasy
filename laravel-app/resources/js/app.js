01315b4ebc649659d4bcc6d61f07339c9ff49f5a
priusz <varro.timi@gmail.com>
2024-11-27 16:04:51 +0100

    add infoFadeOut script and refactor thisYear script

--- laravel-app/resources/js/app.js
+++ laravel-app/resources/js/app.js
@@ -1,10 +1,13 @@
 import logoutConfirm from "./script/logoutConfirm.js";
 import loginButton from "./script/loginButton.js";
 import registerButton from "./script/registerButton.js";
-import './script/thisYear.js'
+import getThisYear from "./script/thisYear.js";
+import infoFadeOut from "./script/infoFadeOut.js";
 
 document.addEventListener('DOMContentLoaded', function () {
+    getThisYear();
     logoutConfirm();
     loginButton();
-    registerButton()
+    registerButton();
+    infoFadeOut();
 })
